f9f396ad449ae4638870b31f32ac1b10e6285cb8
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-01-10 15:18:28 -0500

    Current Version
    Added classes to better split up and organize tasks.  Added an option to create a new task after message appears.
    
    List of potential future additions at bottom.

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -4,54 +4,100 @@ import win32gui
 import datetime
 import time
 
+#dateparser.parse(value, settings={'TIMEZONE': 'UTC'})
 
+class TimmeFormatError(Exception):
+    pass
 
-def user_input():
-    print("Please select a time.")
-    print("Must be in 24hr format.")
-    time = input()
-    minute = int(time[-2:])
-    hour = int(time[:-2])
-    print("Select your message.")
-    message = input()
-    print("You want to say:", message, "at", time, "?")
-    answer = input().lower()
-
-    if answer.startswith("y"):
-        return hour, minute, message
-    else:
-        print("Let's try again")
-        
-
-
-def create_job(scheduler, window, hour, minute, message):
-    scheduler.add_job(run_script, 'cron', hour=hour, minute=minute, args=[window, message])
-
-                         
-
-def run_script(window, message):
-    """Displays provided message"""
-    win32gui.ShowWindow(window, 1) 
-    print(message)
-          
-
-def main():
-    window = win32console.GetConsoleWindow()
-    scheduler = BlockingScheduler()
+class ScheduleTask():
+    """Schedules a message to be sent via the command prompt at specified time."""
     
-    args = user_input()
-    create_job(scheduler, window, *args)
-    win32gui.ShowWindow(window, 0) 
-    scheduler.start()
+    def __init__(self):
+        self.window = win32console.GetConsoleWindow()
+        self.scheduler = BlockingScheduler()
+
+        
+    def verify_time(self):
+        """Ensures valid time input.  Returns the minute and hour."""
+        while True:
+            try:
+                time = input()
+                minute = int(time[-2:])
+                hour = int(time[:-2])
+                if len(time) > 4:
+                    raise TimeFormatError()
+                if minute < -1 or minute > 59:
+                    raise TimeFormatError()
+                if hour < -1 or hour > 23:
+                    raise TimeFormatError()
+            except:
+                print("Incorrect value.  Please try again in HHMM format.")
+                continue
+            else:
+                return minute, hour
+
+    def user_input(self):
+        while True:
+            # Get and verify time
+            print("Please select a time.")
+            print("Must be in 24hr format.")
+            minute, hour = self.verify_time()
+            
+            # Get and verify message
+            print("Select your message.")
+            message = input()
+
+            # Confirm message
+            print("You want to say:")
+            print(message, "at", str(hour)+str(minute) + "?")
+            answer = input().lower()
+            if answer.startswith("y"):
+                return hour, minute, message
+            else:
+                print("Restarting questions...")
+
+    def send_msg(self, message):
+        """Opens window and displays message."""
+        win32gui.ShowWindow(self.window, 1) 
+        print(message, '\n')
+        self.new_message_prompt()
+        
+                
+    def create_task(self):
+        """Creates a task."""
+        hour, minute, message = self.user_input()
+        self.scheduler.add_job(self.send_msg, 'cron', hour=hour, minute=minute, args=[message])
+
+
+    def run_tasks(self):
+        """Hides window and runs created tasks."""
+        win32gui.ShowWindow(self.window, 0) 
+        self.scheduler.start()
+
+        
+    def new_message_prompt(self):
+        """Prompt user to create a new message."""
+        print("Create a new message?")
+        if answer.startswith("y"):
+            self.create_task()
+            self.run_tasks()
+        
+            
+def main():
+    task = ScheduleTask()
+    task.create_task()
+    task.run_tasks()
 
 
 if __name__ == "__main__":
     main()
 
-# Possible additions
-# GUI to display messages instead of CMD
-# Other scheduled tasks besides messages
-
-
 # Hiding and Showing Windows: https://stackoverflow.com/questions/67215357/hide-a-python-caused-window
+# Put message in a windows pop-up box instead
+# Setting up multiple tasks
+# View scheduled tasks
+# Running in the hidden icons section of start bar
+# GUI options
+
+
 
